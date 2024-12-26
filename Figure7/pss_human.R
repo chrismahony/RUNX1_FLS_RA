55698a952a03583ca4dda30d46f432804d1fe8a4
mahonyc <65592074+chrismahony@users.noreply.github.com>
2024-12-26 10:50:08 +0000

    Create pss_human.R

---
+++ Figure7/pss_human.R
@@ -0,0 +1,17 @@
+
+Idents(pss_obj) <- 'gross_celltype'
+cols_umap <- ArchR::paletteDiscrete(pss_obj@meta.data[, "gross_celltype"])
+DimPlot(pss_obj, cols=cols_umap)
+
+fibs_pss <- subset(pss_obj, idents="Fibroblast")
+
+Idents(fibs_pss) <- 'subcluster'
+cols_umap <- ArchR::paletteDiscrete(fibs_pss@meta.data[, "subcluster"])
+
+
+ncol(fibs_pss)
+cols_df <- colours$cluster %>% as.data.frame()
+DimPlot(fibs_pss, cols=c("#208A42", "#F47D2B", "#89288F","#D51F26", "#272E6A" ))
+
+
+

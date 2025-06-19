f0eda3aef3c6806ad171b77894a5306154fda786
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-06-19 14:48:49 +0100

    Create plot_corr_visium_krenn.R

---
+++ Figure2/plot_corr_visium_krenn.R
@@ -0,0 +1,15 @@
+setwd("/rds/projects/c/croftap-runx1data01/analysis_of_public_data_sets/visium_RUNX1_corr")
+library(readxl)
+data <- read_excel("data.xlsx")
+
+ggscatter(data, x = "RUNX1 expression", y = "Krenn_Global",
+          add = "reg.line",
+          conf.int = TRUE,
+          add.params = list(color = "blue", fill = "lightgray"),
+          shape = 21,             # Hollow circles
+          color = "black",        # Border color
+          fill = "white",         # Inside fill color (optional)
+          size = 2 ,
+           stroke = 1.6# Adjust size as needed
+) +
+  stat_cor(method = "pearson", label.x = 1, label.y = 1)

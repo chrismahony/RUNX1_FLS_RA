c843ba45fa7f242be24ab7913250880bb54be293
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-06-20 09:43:12 +0100

    Update organoid_analysis.R

--- Figure6/organoid_analysis.R
+++ Figure6/organoid_analysis.R
@@ -463,3 +463,50 @@ all_data$condition<- as.factor(all_data$condition)
 t.test(b.mean ~ condition, all_data, paired=F)
 ```
 
+```{r}
+
+all_data
+
+summary_data <- all_data %>%
+  dplyr::group_by(condition) %>%
+  dplyr::summarise(
+    Mean = mean(b.mean),
+    SD = sd(b.mean)
+  )
+
+t_res <- t.test(b.mean ~ condition, data = all_data)
+
+p_val <- t_res$p.value
+sig_label <- case_when(
+  p_val < 0.001 ~ "***",
+  p_val < 0.01  ~ "**",
+  p_val < 0.05  ~ "*",
+  TRUE          ~ "ns"
+)
+
+y_max <- max(all_data$b.mean, na.rm = TRUE)
+
+
+summary_data$condition <- factor(summary_data$condition, levels = c("noEC", "EC"))
+all_data$condition <- factor(all_data$condition, levels = c("noEC", "EC"))
+
+summary_data %>% 
+ggplot( aes(x = condition, y = Mean)) +
+  geom_bar(stat = "identity", color = "black", width = 0.8, fill=c("red", "lightgrey")) +   geom_jitter(data = all_data, aes(x = condition, y = b.mean), width = 0.15,         # Points
+              color = "black", size = 2.5, alpha = 0.6) +            # Bars
+  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2, size=1) +        # Error bars
+    theme_minimal() +
+theme(
+      panel.background = element_blank(),       # remove inner panel background
+      plot.background = element_blank(),        # remove outer background
+      panel.grid.major = element_blank(),       # remove major grid lines
+      panel.grid.minor = element_blank(),       # remove minor grid lines
+      axis.line = element_line(color = "black"), # keep axis lines
+    plot.margin = margin(t = 5, r = 5, b = 0, l = 5)
+      ) +
+  annotate("text", 
+           x = 1.5,  # midpoint between bar 1 and 2
+           y = y_max + 0.01,  # space above highest point
+           label = sig_label,
+           size = 6)
+```

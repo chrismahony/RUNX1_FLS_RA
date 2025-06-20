590c3711ef9d3e63e0fbd6259dedb4bd3dbc91ec
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-06-20 09:31:24 +0100

    Update Plotting_flow_graphs.R

--- Figure4/Plotting_flow_graphs.R
+++ Figure4/Plotting_flow_graphs.R
@@ -29,20 +29,34 @@ sig_label <- case_when(
 y_max <- max(analysis_R_f$Percentage, na.rm = TRUE)
   
 
-print(summary_data %>% filter(Cell == unique(summary_data$Cell)[i]) %>% 
-ggplot( aes(x = Condition, y = Mean)) +
-  geom_bar(stat = "identity", color = "black", width = 0.95, fill=c("lightgrey", "red")) +               # Bars
-  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2) +        # Error bars
-  geom_jitter(data = analysis_R_f, aes(x = Condition, y = Percentage), width = 0.15,         # Points
-              color = "black", size = 2.5, alpha = 0.8) +
-  theme_minimal() +
-  labs(title = "Bar Plot with Individual Points and SD",
-       y = "Measured Value")+ggtitle(unique(analysis_R_f$Cell))
-+theme_ArchR()+
-  annotate("text", 
-           x = 1.5,  # midpoint between bar 1 and 2
-           y = y_max + 0.5,  # space above highest point
-           label = sig_label,
-           size = 6))
+print(
+  summary_data %>%
+    filter(Cell == unique(summary_data$Cell)[i]) %>%
+    ggplot(aes(x = Condition, y = Mean)) +
+    geom_bar(stat = "identity", color = "black", width = 0.8, fill = c("lightgrey", "red")) +  # Bars
+    geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2, size = 0.8) +  # Error bars
+    geom_jitter(data = analysis_R_f, aes(x = Condition, y = Percentage), 
+                width = 0.15, color = "black", size = 3.5, alpha = 0.8) +  # Points
+    theme(
+      panel.background = element_blank(),       # remove inner panel background
+      plot.background = element_blank(),        # remove outer background
+      panel.grid.major = element_blank(),       # remove major grid lines
+      panel.grid.minor = element_blank(),       # remove minor grid lines
+      axis.line = element_line(color = "black"), # keep axis lines
+    plot.margin = margin(t = 5, r = 5, b = 0, l = 5)
+      ) +
+    labs(
+      title = "Bar Plot with Individual Points and SD",
+      y = "Measured Value"
+    ) +
+    ggtitle(unique(analysis_R_f$Cell)) +
+    annotate(
+      "text",
+      x = 1.5,
+      y = y_max + 0.5,
+      label = sig_label,
+      size = 6
+    )
+)
 
 }

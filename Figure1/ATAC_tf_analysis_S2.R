f0b635fc8868a2314b670c1788fe3695ce674f99
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-07-10 18:51:04 +0100

    Update ATAC_tf_analysis_S2.R

--- Figure1/ATAC_tf_analysis_S2.R
+++ Figure1/ATAC_tf_analysis_S2.R
@@ -1,15 +1,15 @@
+
+
 DefaultAssay(obj.pair_all_coembed_merge_nomural) <- 'chromvar'
 Idents(obj.pair_all_coembed_merge_nomural) <- 'cluster.name'
 All_markers_TFs <- FindAllMarkers(obj.pair_all_coembed_merge_nomural, only.pos=T)
-
-
-write.csv(All_markers_TFs, "/rds/projects/m/mahonyc-cesar-data/fibro_analysis/TF_subcluster/motifs.csv")
 
 rm(list=ls()[! ls() %in% c("All_markers_TFs","obj.pair_all_coembed_merge_nomural")])
 
 All_markers_TFs_f <- All_markers_TFs %>% filter(p_val_adj < 0.05) %>%
     group_by(cluster) %>%
       slice_head(n = 3)
+
 
 colnames(jaspar2) <- c('ID','gene')
 
@@ -18,7 +18,7 @@ All_markers_TFs_f$gene_name <- jaspar2$gene[index]
 rm(index)
 
 
-dotplot<-DotPlot(obj.pair_all_coembed_merge_nomural, features = unique(All_markers_TFs_f$gene))
+dotplot<-DotPlot(obj.pair_all_coembed_merge_nomural, features = unique(All_markers_TFs_f$gene), idents=levels(obj.pair_all_coembed_merge_nomural)[-8])
 
 dotplot<-dotplot$data
 
@@ -42,8 +42,26 @@ dotplot <- dotplot[,-1] %>% as.matrix()
 
 library(ComplexHeatmap)
 
-Heatmap(dotplot[,c(3,8, 4, 7, 6, 1, 2, 9, 10, 5)], border=T, cluster_columns = F, col=viridis(100))
+Heatmap(dotplot)
 
-ncol(dotplot)
+library(ComplexHeatmap)
+library(viridis)
+library(circlize)
 
-heatmap(dotplot)
+# Define color function
+col_fun <- colorRamp2(
+  c(min(dotplot), mean(dotplot), max(dotplot)),
+  viridis(3)
+)
+
+Heatmap(
+  dotplot[,c(3,4,7,6,1,2,8,9,5)],
+  col = col_fun,
+  cluster_rows = TRUE,
+  cluster_columns = F,
+  show_row_names = FALSE,
+  show_column_names =T,
+  border=T
+)
+
+Heatmap(dotplot[,c(3,4,7,6,1,2,8,9,5)], border=T, cluster_columns = F, col=viridis(100))

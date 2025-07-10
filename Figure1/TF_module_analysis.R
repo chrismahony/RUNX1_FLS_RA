42ee8728824e35f2362bd8edd546b55854a9f478
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-07-10 16:04:35 +0100

    Update TF_module_analysis.R

--- Figure1/TF_module_analysis.R
+++ Figure1/TF_module_analysis.R
@@ -1,4 +1,8 @@
-grn$Target <- gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(grn$Target), perl=TRUE)
+grn <- read.csv("/rds/projects/m/mahonyc-cesar-data/fibro_analysis/scMEGA_repeat/df_grn2_new_FINAL.csv")
+
+grn$Target <- gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(grn$gene), perl=TRUE)
+
+grn$Source <- grn$tf
 
 tfs <- unique(grn$Source)
 grn_list <- list()
@@ -14,9 +18,12 @@ stia2021_rna <- AddModuleScore(stia2021_rna, features = list(grn_list[[i]]$Targe
 to_plot <- colnames(stia2021_rna@meta.data)[grep("TF_", colnames(stia2021_rna@meta.data))]
 
 Idents(stia2021_rna) <- 'cluster.name'
-levels(stia2021_rna)[-c(2,3,12)]
+#levels(stia2021_rna)[-c(2,3,5,12)]
+#stia2021_rna_f <- subset(stia2021_rna, idents=levels(stia2021_rna)[-c(2,3,5,12)])
 
-dotplot<-DotPlot(stia2021_rna, features= to_plot, idents = levels(stia2021_rna)[-c(2,3,5)])
+
+
+dotplot<-DotPlot(stia2021_rna, features= to_plot, idents = levels(stia2021_rna)[-c(2,3,5,12)])
 
 dotplot<-dotplot$data
 library(tidyr)
@@ -35,7 +42,7 @@ library(ComplexHeatmap)
 
 ann <- colnames(dotplot) %>% as.data.frame()
 colnames(ann) <- 'clusters'
-ann$condition <- c("lining", rep("sublining",9))
+ann$condition <- c("lining", rep("sublining",8))
 
 
 library(ComplexHeatmap)
@@ -46,16 +53,15 @@ col_ann <- HeatmapAnnotation(df = ann[,c("condition") ], col=colours)
 
 
 Heatmap(dotplot, row_names_gp = gpar(fontsize = 5), top_annotation = col_ann)
-Heatmap(dotplot, row_names_gp = gpar(fontsize = 5))
+Heatmap(dotplot, row_names_gp = gpar(fontsize = 5), border=T)
 
 
 
 
+dotplto_1 <- dotplot %>% as.data.frame() %>%  filter(fibroblast_sublining_C1qtnf3_Col8a1 > 0.1 & fibroblast_sublining_Fmo2 > 0.1 & fibroblast_sublining_Sfrp1_Cfb > 0.1 & fibroblast_sublining_lining_Ccl7_Ccl2 > 0.1)
 
 
-dotplto_1 <- dotplot %>% as.data.frame() %>%  filter(fibroblast_sublining_C1qtnf3_Col8a1 > 0.1 & fibroblast__Chodl > 0.1 & fibroblast_sublining_Sfrp1_Cfb > 0.1 & fibroblast_sublining_lining_Ccl7_Ccl2 > 0.1)
-
-dotplto_2 <- dotplot %>% as.data.frame() %>%  filter(fibroblast_sublining_Fmo2 > 0.1 & fibroblast_sublining_Serpina3c_C3 > 0.1 & fibroblast_sublining_Ccl11 > 0.1 & fibroblast_sublining_Pi16 > 0.1)
+dotplto_2 <- dotplot %>% as.data.frame() %>%  filter(fibroblast_sublining_Serpina3c_C3 > 0.1 & fibroblast_sublining_Ccl11 > 0.1 & fibroblast_sublining_Pi16 > 0.1)
 
 dotplto_3 <- dotplot %>% as.data.frame() %>%  filter(fibroblast__Crabp1_Col23a1 > 0.1)
 
@@ -113,43 +119,5 @@ colnames(ann) <- 'clusters'
 colours <- list('clusters'= ArchR::paletteDiscrete(ann$clusters))
 col_ann <- HeatmapAnnotation(df = ann, col=colours)
 
-Heatmap(dotplot[,c(1,3,6,2,4,5)], row_names_gp = gpar(fontsize = 5), cluster_columns = F,top_annotation = col_ann)
-
-
-tf_1_genes$group <- 'mod1'
-tf_2_genes$group <- 'mod2'
-tf_3_genes$group <- 'mod3'
-tf_4_genes$group <- 'mod4'
-
-all_tfs <- rbind(tf_1_genes, tf_2_genes, tf_3_genes,tf_4_genes)
-
-g <- graph_from_data_frame(all_tfs, directed = TRUE)
-
-degree_df <- data.frame(
-  Node = V(g)$name,
-  In_Degree = degree(g, mode = "in"),
-  Out_Degree = degree(g, mode = "out")
-)
-
-index <- match(degree_df$Node, all_tfs$Source)
-degree_df$cluster <- all_tfs$group[index]
-
-
-degree_df_top <- degree_df %>%
-  group_by(cluster) %>% 
-  slice_head(n = 40) 
-
-library(ggwordcloud)
-
-gg_wc <- ggplot(degree_df_top,
-                aes(label = Node, 
-                    size = Out_Degree,
-                    color = cluster)) +
-  geom_text_wordcloud(shape = "square") +
-  scale_size_area(max_size = 12) +
-  #scale_color_manual(values = mycolors) +
-  theme_minimal() +
-  facet_wrap(~cluster)
-
-
+Heatmap(dotplot[,c(1,3,6,2,4,5)], row_names_gp = gpar(fontsize = 5), cluster_columns = F,top_annotation = col_ann, border=T)
 

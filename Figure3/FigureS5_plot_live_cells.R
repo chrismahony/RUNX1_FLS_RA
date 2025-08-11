67c46ef164b28375e7250722caff010975399c4d
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-08-11 17:32:00 +0100

    Update FigureS5_plot_live_cells.R

--- Figure3/FigureS5_plot_live_cells.R
+++ Figure3/FigureS5_plot_live_cells.R
@@ -27,16 +27,17 @@ measurments$Condition <- factor(measurments$Condition, levels = c("DMSO", "CBFβ
 
 summary_data %>% 
 ggplot( aes(x = Condition, y = Mean)) +
-  geom_bar(stat = "identity", color = "black", width = 0.8, fill=c("red", "lightgrey")) +               # Bars
-  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2, size = 0.8) +        # Error bars
-  geom_jitter(data = measurments, aes(x = Condition, y = `Live cells`), width = 0.25,         # Points
-              color = "black", size = 3.5, alpha = 0.8) +
+  geom_bar(stat = "identity", color = c("red", "black"), width = 0.65, fill="white") +               # Bars
+  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.3, color = c("red", "black")) +        # Error bars
+  geom_jitter(data = measurments, aes(x = Condition, y = `Live cells`, color = Condition), width = 0.25,         # Points
+               size = 3, alpha = 1)+
+  scale_color_manual(values = c("DMSO" = "black", "CBFβi" = "red")) +
    theme(
       panel.background = element_blank(),       # remove inner panel background
       plot.background = element_blank(),        # remove outer background
       panel.grid.major = element_blank(),       # remove major grid lines
       panel.grid.minor = element_blank(),       # remove minor grid lines
-      axis.line = element_line(color = "black"), # keep axis lines
-    plot.margin = margin(t = 5, r = 5, b = 0, l = 5)
-      ) 
+      axis.line = element_line(color = "black")
+      ) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05)))+NoLegend()
 

e87e67f02f4f28dc651895b559a12410b517d088
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-08-11 17:48:43 +0100

    Update plot_QPCR_sal_gland.R

--- Figure7/plot_QPCR_sal_gland.R
+++ Figure7/plot_QPCR_sal_gland.R
@@ -32,10 +32,10 @@ qPCR_data$Condition <- factor(qPCR_data$Condition, levels = c("ctrl", "-Runx1"))
 
 summary_data %>% 
   ggplot(aes(x = Condition, y = Mean)) +
-  geom_bar(stat = "identity", color = "black", width = 0.8, fill = c("red", "grey")) +  # Bars
-  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2, size = 0.8) +          # Error bars
-  geom_jitter(data = qPCR_data, aes(x = Condition, y = `2_neg_d_CTx100000`), width = 0.15,       # Points
-              color = "black", size = 3.5, alpha = 0.8) +
+  geom_bar(stat = "identity", color = c("red", "black"), fill = "white", width = 0.55) +  # Bars
+  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.3, size = 0.8, color = c("red", "black")) +          # Error bars
+  geom_jitter(data = qPCR_data, aes(x = Condition, y = `2_neg_d_CTx100000`, color = Condition), width = 0.25,       # Points
+              size = 3, alpha = 1) +
   theme(
     panel.background = element_blank(),       # remove inner panel background
     plot.background = element_blank(),        # remove outer background
@@ -44,8 +44,10 @@ summary_data %>%
     axis.line = element_line(color = "black"),# keep axis lines
     plot.margin = margin(t = 5, r = 5, b = 0, l = 5)
   ) +
+  scale_color_manual(values = c("ctrl" = "black", "-Runx1" = "red")) +
   annotate("text", 
            x = 1.5,  # midpoint between bar 1 and 2
            y = y_max + 0.1,  # space above highest point
            label = sig_label,
-           size = 6) 
+           size = 6) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05)))+NoLegend()

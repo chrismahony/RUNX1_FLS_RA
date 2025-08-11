16040e8178338e8ec5a2d412239588c41d3e66e5
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-08-11 17:10:50 +0100

    Update Plot_luciferase.R

--- Figure5/Plot_luciferase.R
+++ Figure5/Plot_luciferase.R
@@ -1,5 +1,6 @@
 setwd("/rds/projects/c/croftap-runx1data01/luciferase_analysis")
 library(readxl)
+
 data <- read_excel("data.xlsx")
 
 data$Condition_DOX <- paste(data$Condition, data$DOX, sep="_")
@@ -46,22 +47,24 @@ print(
   summary_data %>%
   filter(Gene == unique(summary_data$Gene)[i]) %>%
   ggplot(aes(x = Condition_DOX, y = Mean)) +
-  geom_bar(stat = "identity", color = "black", width = 0.8, 
-           fill = c("lightgrey", "lightgrey", "red", "red")) +  # Bars
-  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2, size = 0.8) +  # Error bars
+  geom_bar(stat = "identity", color = c("black", "black", "red", "red"), width = 0.55, 
+           fill = c("white", "white", "white", "white")) +  # Bars
+  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.3, color = c("black", "black", "red", "red")) +  # Error bars
   geom_jitter(data = analysis_R_f, 
-              aes(x = Condition_DOX, y = `Normalised Luciferase Activity`), 
-              width = 0.15, color = "black", size = 3.5, alpha = 0.8) +  # Remove gap between bar and x-axis
+              aes(x = Condition_DOX, y = `Normalised Luciferase Activity`,color = Condition_DOX), 
+              width = 0.25,  size = 3, alpha = 1, ) +
+    scale_color_manual(values = c("EV_No_DOX" = "black", "EV_With_DOX" = "black", "RUNX1c_No_DOX" = "red", "RUNX1c_With_DOX" = "red"))+
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05)))+# Remove gap between bar and x-axis
   theme(
     panel.background = element_blank(),       # remove inner panel background
     plot.background = element_blank(),        # remove outer background
     panel.grid.major = element_blank(),       # remove major grid lines
     panel.grid.minor = element_blank(),       # remove minor grid lines
-    axis.line = element_line(color = "black"), # keep axis lines
-    plot.margin = margin(t = 5, r = 5, b = 0, l = 5)
+    axis.line = element_line(color = "black")
   ) +
-    ggtitle(unique(analysis_R_f$Gene)) 
+    ggtitle(unique(analysis_R_f$Gene)) +NoLegend()
 )
+
 
 }
 

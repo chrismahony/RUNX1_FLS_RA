ac7a6d77529d3e4c9d0b49dc58352c4d1502514b
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-08-12 18:08:46 +0100

    Update plot_salivary_glad_flow.R

--- Figure7/plot_salivary_glad_flow.R
+++ Figure7/plot_salivary_glad_flow.R
@@ -32,18 +32,17 @@ y_max <- max(analysis_R_f$Percentage, na.rm = TRUE)
 
 print(summary_data %>% dplyr::filter(Cell == unique(summary_data$Cell)[i]) %>% 
 ggplot( aes(x = Condition, y = Mean)) +
-  geom_bar(stat = "identity", color = "black", width = 0.95, fill=c("lightgrey", "red")) +               # Bars
-  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2) +        # Error bars
-  geom_jitter(data = analysis_R_f, aes(x = Condition, y = Percentage), width = 0.15,         # Points
-              color = "black", size = 2.5, alpha = 0.8) +  # Points
+  geom_bar(stat = "identity", color = c("black", "red"), width = 0.6, fill=c("white")) +               # Bars
+  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2, color=c("black", "red")) +        # Error bars
+  geom_jitter(data = analysis_R_f, aes(x = Condition, y = Percentage, color=Condition), width = 0.15, size = 2.5, alpha = 1) +  # Points
     theme(
       panel.background = element_blank(),       # remove inner panel background
       plot.background = element_blank(),        # remove outer background
       panel.grid.major = element_blank(),       # remove major grid lines
       panel.grid.minor = element_blank(),       # remove minor grid lines
-      axis.line = element_line(color = "black"), # keep axis lines
-    plot.margin = margin(t = 5, r = 5, b = 0, l = 5)
+      axis.line = element_line(color = "black")
       )+
+  scale_color_manual(values = c("Ctrl" = "black", "TAM" = "red")) +
   labs(title = "Bar Plot with Individual Points and SD",
        y = "Measured Value")+ggtitle(unique(analysis_R_f$Cell))
 +
@@ -51,31 +50,5 @@ ggplot( aes(x = Condition, y = Mean)) +
            x = 1.5,  # midpoint between bar 1 and 2
            y = y_max + 0.5,  # space above highest point
            label = sig_label,
-           size = 6))
-
-
-
-print(summary_data %>% dplyr::filter(Cell == unique(summary_data$Cell)[i]) %>% 
-  ggplot(aes(x = Condition, y = Mean)) +
-  geom_bar(stat = "identity", color = "black", width = 0.95, fill = "lightgrey")  +
-  geom_jitter(data = analysis_R_f,
-              aes(x = Condition, y = Percentage, fill = Condition),
-              width = 0.15,
-              shape = 21,            # Needed to allow fill + border
-              color = "black",       # Border color
-              size = 3.5,
-              alpha = 1) +
-    geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2)+
-  theme_minimal() +
-  theme_ArchR() +
-  annotate("text", 
-           x = 1.5,
-           y = y_max + 0.001,
-           label = sig_label,
-           size = 6) + scale_fill_manual(values = c("TAM" = "#E41A1C", "Ctrl" = "#377EB8"))+
-  labs(title = "Bar Plot with Individual Points and SD",
-       y = "Measured Value")+ggtitle(unique(analysis_R_f$Cell)))
-
-
-
-}
+           size = 6)+
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05)))+NoLegend())

d15088ed9ce2ae7b5f14797e2a28f3b50724d2fe
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-08-12 10:22:24 +0100

    Update PLot_RUNX1_multiplex_imaging.R

--- Figure3/PLot_RUNX1_multiplex_imaging.R
+++ Figure3/PLot_RUNX1_multiplex_imaging.R
@@ -40,20 +40,19 @@ RUNX1_intenst$Condition <- factor(RUNX1_intenst$cat, levels = c("Unstimulated", 
 
 summary_data %>%  
 ggplot( aes(x = cat, y = Mean)) +
-  geom_bar(stat = "identity", color = "black", width = 0.8, fill=c("red", "lightgrey")) +               # Bars
-  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2, size = 0.8) +        # Error bars
-  geom_jitter(data = RUNX1_intenst, aes(x = cat, y = RUNX1), width = 0.15,         # Points
-              color = "black", size = 3.5, alpha = 0.8) +
+  geom_bar(stat = "identity", color = c("red", "black"), fill = "white", width = 0.55) +               # Bars
+  geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.3, size = 0.8, color = c("red", "black")) +        # Error bars
+  geom_jitter(data = RUNX1_intenst, aes(x = cat, y = RUNX1, color = cat), width = 0.25,         # Points
+              size = 3, alpha = 1) +
    theme(
       panel.background = element_blank(),       # remove inner panel background
       plot.background = element_blank(),        # remove outer background
       panel.grid.major = element_blank(),       # remove major grid lines
       panel.grid.minor = element_blank(),       # remove minor grid lines
-      axis.line = element_line(color = "black"), # keep axis lines
-    plot.margin = margin(t = 5, r = 5, b = 0, l = 5)
-      ) +
+      axis.line = element_line(color = "black")
+      ) +scale_color_manual(values = c("low" = "black", "high" = "red")) +
   annotate("text", 
            x = 1.5,  # midpoint between bar 1 and 2
            y = y_max + 0.5,  # space above highest point
            label = sig_label,
-           size = 6)
+           size = 6)+NoLegend()

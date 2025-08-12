41b490f9ffe6fbb46287d8ef05e9423a920f6321
mahonyc <65592074+chrismahony@users.noreply.github.com>
2025-08-12 18:48:09 +0100

    Update Plotting_flow_graphs.R

--- Figure4/Plotting_flow_graphs.R
+++ Figure4/Plotting_flow_graphs.R
@@ -33,18 +33,18 @@ print(
   summary_data %>%
     filter(Cell == unique(summary_data$Cell)[i]) %>%
     ggplot(aes(x = Condition, y = Mean)) +
-    geom_bar(stat = "identity", color = "black", width = 0.8, fill = c("lightgrey", "red")) +  # Bars
-    geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.2, size = 0.8) +  # Error bars
-    geom_jitter(data = analysis_R_f, aes(x = Condition, y = Percentage), 
-                width = 0.15, color = "black", size = 3.5, alpha = 0.8) +  # Points
+    geom_bar(stat = "identity", color = c("black","red"), width = 0.6, fill = c("white")) +  # Bars
+    geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0.3, color = c("black","red")) +  # Error bars
+    geom_jitter(data = analysis_R_f, aes(x = Condition, y = Percentage, color=Condition), 
+                width = 0.15, size = 3, alpha = 1) +  # Points
     theme(
       panel.background = element_blank(),       # remove inner panel background
       plot.background = element_blank(),        # remove outer background
       panel.grid.major = element_blank(),       # remove major grid lines
       panel.grid.minor = element_blank(),       # remove minor grid lines
-      axis.line = element_line(color = "black"), # keep axis lines
-    plot.margin = margin(t = 5, r = 5, b = 0, l = 5)
+      axis.line = element_line(color = "black")
       ) +
+  scale_color_manual(values = c("Control" = "black", "Exp" = "red")) +
     labs(
       title = "Bar Plot with Individual Points and SD",
       y = "Measured Value"
@@ -56,7 +56,8 @@ print(
       y = y_max + 0.5,
       label = sig_label,
       size = 6
-    )
+    )+
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05)))+NoLegend()
 )
 
 }
